9c043d8fdae729da433c24f1c74f65ac6d88f478
Palmeirense312 <142997886+Palmeirense312@users.noreply.github.com>
2023-08-23 09:48:13 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+var Times=[
+"Campeonato_Brasileiro_de_Futebol_de_2022_-_Série_A.png","Palmeiras_logo.svg.png","Santos_logo.png","i.png","Flamengo_braz_logo.svg.png","Brasao_do_Sao_Paulo_Futebol_Clube.svg.png"
+
+];
+var nome=[
+    "Maiores campeões do brasileirão", "Palmeiras (11 titulos)", "Santos (8 titulos)", "Corinthians (7 titulos)", "Flamengo (7 titulos)","São Paulo (6 titulos)"
+];
+var i= 0
+function update()
+{
+i++;
+var paulinholoko = 4
+if(i > paulinholoko)
+{
+    i = 0
+}
+var atualizarImagem = Times[i]
+var atualizarNome = nome[i]
+document.getElementById("imagens").src = atualizarImagem;
+document.getElementById("nomes").innerHTML = atualizarNome;
+
+}
